8ff19c415d2c8b75416b2fb40ab1bd13cd17f8c8
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2021-04-20 18:21:59 +0200

    Step_07 Semplificazione della tabella delle città (senza temperature)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { OpenweatherService } from './openweather.service';
 
-class City {
-  nome: string;
-  temperatura: string
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,30 +8,18 @@ class City {
 })
 export class AppComponent {
   title: string = 'Esercizio Angular';
-  cities: Array<City> = [
-    {
-      nome: 'Torino',
-      temperatura: '14'
-    }, {
-      nome: 'Milano',
-      temperatura: '15'
-    }, {
-      nome: 'Genova',
-      temperatura: '18'
-    }
+  cities: Array<string> = ['Torino','Milano','Genova'
   ];
-  selezionata: City;
+  selezionata: string;
+  temperatura: string;
   constructor(private wbs: OpenweatherService) { }
   clean() {
-    this.selezionata=undefined;
+    this.selezionata = undefined;
   }
   refreshTemperature(itemName: string) {
-    var trovato: Array<City> = this.cities.filter(
-      el => ( el.nome === itemName )
-    );
-    this.selezionata = trovato[0];
-    this.wbs.getData(this.selezionata.nome).subscribe(
-      ( x: any ) => this.selezionata.temperatura = x.data[0].temp,
+    this.selezionata = itemName;
+    this.wbs.getData(itemName).subscribe(
+      ( x: any ) => this.temperatura = x.data[0].temp,
       err => console.error('Observer got an error: ' + err)
     );
  }
